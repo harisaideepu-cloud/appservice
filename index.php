3f94814515ba95f172a14c34289d38c485715f43
harisaideepu-cloud <harisaideepu1@gmail.com>
2025-03-05 12:51:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// PHP code to include embedded GIFs
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Embedded GIFs with PHP</title>
+</head>
+<body>
+  <h1>Check out these awesome GIFs!</h1>
+
+  <!-- First Embedded GIF -->
+  <div class="tenor-gif-embed" data-postid="2229982983562623658" data-share-method="host" data-aspect-ratio="1.81752" data-width="100%">
+    <a href="https://tenor.com/view/adhurs-sunil-raviteja-bramhi-rey-thislsuday-gif-2229982983562623658">Adhurs Sunil GIF</a> from 
+    <a href="https://tenor.com/search/adhurs-gifs">Adhurs GIFs</a>
+  </div>
+  <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+  <!-- Second Embedded GIF -->
+  <div class="tenor-gif-embed" data-postid="19217874" data-share-method="host" data-aspect-ratio="0.7125" data-width="100%">
+    <a href="https://tenor.com/view/bala-throws-phone-ballaya-krishna-gif-19217874">Bala Throws GIF</a> from 
+    <a href="https://tenor.com/search/bala-gifs">Bala GIFs</a>
+  </div>
+  <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+</body>
+</html>
+';
+?>
